76fa506eb2d0b6eb5a5418bd2c04cc55dcd54683
Michael Stoltz <mstoltz515@gmail.com>
2016-03-13 22:50:46 -0400

    make dummy app

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the home page route
+app.get('/', function(req, res) {
+    // ejs render automatically looks in the views folder
+    res.json({
+      notifications: [{
+        title: "test notification",
+        description: "this is a notification",
+        time: new Date().getTime()
+      }]
+    });
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
